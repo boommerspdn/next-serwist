9d76eac3de5ea54b9100300019a8b2afa04182ea
boommerspdn <131756488+boommerspdn@users.noreply.github.com>
2025-06-13 21:29:36 +0700

    test ssr

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,29 @@ export const metadata: Metadata = {
   title: "Home",
 };
 
-export default function Page() {
+async function getPosts() {
+  const res = await fetch(
+    "https://jsonplaceholder.typicode.com/posts?_limit=5",
+    {
+      cache: "no-store", // for SSR
+    },
+  );
+  return res.json();
+}
+
+export default async function Page() {
+  const posts = await getPosts();
   return (
-    <>
-      <h1>Next.js + Serwist</h1>
-    </>
+    <div>
+      <h1>Posts from JSONPlaceholder</h1>
+      <ul>
+        {posts.map((post: any) => (
+          <li key={post.id}>
+            <strong>{post.title}</strong>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
