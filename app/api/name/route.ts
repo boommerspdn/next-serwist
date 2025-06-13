3ecb8ef60939d588db9f0ca662f3242611542bd7
boommerspdn <131756488+boommerspdn@users.noreply.github.com>
2025-06-14 00:06:18 +0700

    fetching no dynamic

--- app/api/name/route.ts
+++ app/api/name/route.ts
@@ -2,7 +2,6 @@ import { NextResponse } from "next/server";
 import prisma from "./../../../lib/prisma";
 
 export async function GET() {
-  return Response.json({
-    message: `GET method called`,
-  });
+  const users = await prisma.user.findMany();
+  return NextResponse.json(users);
 }
